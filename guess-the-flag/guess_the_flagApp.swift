d02b7d240eae77e0f09a6bd50aba58128eb484c8
dangabrielle <hoangabrielle@gmail.com>
2024-03-10 11:50:05 -1000

    Initial Commit

---
+++ guess-the-flag/guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guess_the_flagApp.swift
+//  guess-the-flag
+//
+//  Created by Gabrielle Dang on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
